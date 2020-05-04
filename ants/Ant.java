a7ea0899cf9d40f0765f007d9eeb829f75eadb75
tannerbortel <60672539+tannerbortel@users.noreply.github.com>
2020-05-04 15:24:09 -0700

    Completed Excersise

--- ants/Ant.java
+++ ants/Ant.java
@@ -3,17 +3,23 @@ import greenfoot.*;
 /**
  * An ant that collects food.
  * 
- * @author Michael Kölling
- * @version 0.1
+ * @author Tanner Bortel
+ * @version 0.2
  */
 public class Ant extends Creature
 {
+    private boolean carryingFood;
+    private GreenfootImage image1;
+    private GreenfootImage image2;
     /**
      * Create an ant with a given home hill. The initial speed is zero (not moving).
      */
     public Ant(AntHill home)
     {
         setHomeHill(home);
+        image1 = getImage();
+        image2 = new GreenfootImage("ant-with-food.gif");
+        carryingFood = false;
     }
 
     /**
@@ -21,19 +27,50 @@ public class Ant extends Creature
      */
     public void act()
     {
-        testFoodPile(); // This currently does not do anything
+        status();
     }
-    
-    private void testFoodPile()
+    private void checkForFood()
     {
-    
-    /*****************************************************************
         Food food = (Food) getOneIntersectingObject(Food.class);
-        if (food != null) 
+        if (food != null)
         {
             food.removeCrumb();
+            carryingFood = true;
+            setImage(image2);
         }
-    *****************************************************************/
-    
+    }
+    private boolean atHome()
+    {
+        if (getHomeHill() != null) 
+        {
+            return (Math.abs(getX() - getHomeHill().getX()) < 4) && 
+                   (Math.abs(getY() - getHomeHill().getY()) < 4);
+        }
+        else 
+        {
+             return false;
+        }
+    }
+    private void searchForFood()
+    {
+        randomWalk();
+        checkForFood();
+    }
+    private void status()
+    {
+        if(carryingFood)
+        {
+            walkTowardsHome();
+            if(atHome())
+            {
+                setImage(image1);
+                getHomeHill( ).countFood( );
+                carryingFood = false;
+            }
+        }
+        else
+        {
+            searchForFood();
+        }
     }
 }
